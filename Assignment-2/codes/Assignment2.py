1848c3a80a961867a5c48133f56b91a99c89c920
Satvik <80506149+KoteshSatvik@users.noreply.github.com>
2021-03-24 17:58:17 +0530

    Update Assignment2.py

--- Assignment-2/codes/Assignment2.py
+++ Assignment-2/codes/Assignment2.py
@@ -3,10 +3,14 @@ import numpy as np
 # Let us consider 1000000 cases.
 n = 1000000
 # Given y=0.5.
+# and f(x,y)= 2 for x>0,y>0,x+y<1.
 # Given x>0,y>0 and x+y<1 that implies x < 1-y
 # therefore 0 < x < 1-y
 # implies   0 < x < 1-0.5
 # implies   0 < x < 0.5
+# As f(x,y) is constant for 0 < x < 0.5
+# x has equal chance of taking any value between 0 and 0.5
+
 x = 0.5*np.random.random_sample(n)
 sum = np.sum(x)
 
